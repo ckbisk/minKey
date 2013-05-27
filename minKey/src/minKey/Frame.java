274ed4181ab5f7d4bc0d23a71d224e000178cbb0
Chad Bisk <ckbisk@gmail.com>
2013-05-27 11:20:51 -0400

    full code

---
+++ minKey/src/minKey/Frame.java
@@ -0,0 +1,29 @@
+package minKey;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Frame extends JFrame {
+
+	public static void main(String[] args) {
+		// Use the event dispatch thread to build the UI for thread-safety.
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Frame();
+			}
+		});
+	}
+	
+	private Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		Panel panel = new Panel();
+		setContentPane(panel);
+		setVisible(true);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ minKey/src/minKey/Panel.java
@@ -0,0 +1,37 @@
+package minKey;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.logging.Logger;
+
+import javax.swing.JPanel;
+
+public class Panel extends JPanel {
+
+	public Panel() {
+		setFocusable(true);
+		addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent event) {
+				warn(event.toString());
+			}
+
+			@Override
+			public void keyReleased(KeyEvent event) {
+				warn(event.toString());
+			}
+
+			@Override
+			public void keyPressed(KeyEvent event) {
+				warn(event.toString());
+			}
+		});
+	}
+
+	private void warn(String msg) {
+		Logger.getLogger(getClass().getName()).warning(msg);
+	}
+
+	private static final long serialVersionUID = 1L;
+}
